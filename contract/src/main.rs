6a7baf9c8f02d19e5867c88c12362f1c1ab41f68
ipopescu <ipopescu@users.noreply.github.com>
2022-09-22 14:30:35 +0200

    Updated contract file

--- contract/src/main.rs
+++ contract/src/main.rs
@@ -1,9 +1,8 @@
+#![no_std]
 #![no_main]
 
-use casper_contract::{
-    contract_api::{account, runtime},
-    unwrap_or_revert::UnwrapOrRevert,
-};
+use casper_contract::contract_api::{runtime, account};
+use casper_contract::unwrap_or_revert::UnwrapOrRevert;
 use casper_types::account::{AccountHash, ActionType, Weight};
 
 const ASSOCIATED_ACCOUNT: &str = "deployment-account";
